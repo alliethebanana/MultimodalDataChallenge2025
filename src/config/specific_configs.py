52aae3fb830af161fa3375417757bd0db73a57a6
bmgi <bmgi@dtu.dk>
2025-08-13 07:57:32 +0200

    Adding default config

--- src/config/specific_configs.py
+++ src/config/specific_configs.py
@@ -4,6 +4,9 @@ For getting specific configs
 
 """
 
+
+from src import save_load_json
+
 from src.config.model_config import ModelConfig, MetaDataEmbeddingConfig
 
 
@@ -13,6 +16,21 @@ def make_and_save_default_config():
     """
     metadata_config = MetaDataEmbeddingConfig(
         habitat='default', location='default', substrate='default', event_date='default')
+    
+    model_config = ModelConfig(
+        random_seed=0, 
+        image_embedding_type='default',
+        image_embedding_size=200,
+        unknown_as_token=True,
+        metadata_embedding_type=metadata_config,
+        metadata_embedding_model_before_comb=None,
+        combination_type='concat',
+        classifier_after_combination='default',
+        patience=5)
+    
+    config_path = 'configs/default_model_config.json'
+    
+    save_load_json.save_as_json(model_config, config_path)
     
     
 
